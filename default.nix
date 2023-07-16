0aa9f1508c3f7077e1bebbba72403135f8ef409a
networkuka <87647552+networkuka@users.noreply.github.com>
2023-07-16 12:29:07 +0300

    Add files via upload

---
+++ default.nix
@@ -0,0 +1,100 @@
+{ config, pkgs, user, inputs, ... }:
+
+{
+  imports =
+    (import ../../../modules/hardware) ++
+    [
+      ../hardware-configuration.nix
+      ../../../modules/fonts
+    ] ++ [
+      ../../../modules/desktop/hyprland
+    ];
+
+  users.users.root.initialHashedPassword = "$6$LHGmA/ySyxJ.7kJx$Ub6vs3XUQ8bGA4DcbvJ2BIlm/FL3csQ1QeQG8IjNnZNXe7rEFLpO8BTP4.kSlGgdPej70JEqJABo8EbwvxIfZ1";
+  users.users.${user} = {
+    initialHashedPassword = "$6$LHGmA/ySyxJ.7kJx$Ub6vs3XUQ8bGA4DcbvJ2BIlm/FL3csQ1QeQG8IjNnZNXe7rEFLpO8BTP4.kSlGgdPej70JEqJABo8EbwvxIfZ1";
+    # shell = pkgs.fish;
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" ];
+    packages = (with pkgs; [
+      # chatgpt-cli
+    ]) ++ (with config.nur.repos;[
+      # linyinfeng.icalingua-plus-plus
+    ]);
+  };
+  boot = {
+    supportedFilesystems = [ "ntfs" ];
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        consoleMode = "auto";
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      timeout = 3;
+    };
+    kernelParams = [
+      "quiet"
+      "splash"
+      # "nvidia-drm.modeset=1"
+    ];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-rime fcitx5-chinese-addons fcitx5-table-extra fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki ];
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      libnotify
+      wl-clipboard
+      wlr-randr
+      cinnamon.nemo
+      networkmanagerapplet
+      wev
+      wf-recorder
+      alsa-lib
+      alsa-utils
+      flac
+      pulsemixer
+      imagemagick
+      pkgs.sway-contrib.grimshot
+      flameshot
+      grim
+    ];
+  };
+
+  services = {
+    dbus.packages = [ pkgs.gcr ];
+    getty.autologinUser = "${user}";
+    gvfs.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+  };
+
+  security.polkit.enable = true;
+  security.sudo = {
+    enable = true;
+    extraConfig = ''
+      ${user} ALL=(ALL) NOPASSWD:ALL
+    '';
+  };
+  security.doas = {
+    enable = false;
+    extraConfig = ''
+      permit nopass :wheel
+    '';
+  };
+
+}
